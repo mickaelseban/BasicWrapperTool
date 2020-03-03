fc1eca9806a6251d85c619ad741a45345fe7d9e5
seban <seban@DESKTOP-N706T72>
2020-03-03 02:50:56 +0000

    WIP: update tests

--- test/BasicWrapperTool.Tests/ResultExtensionsTests.cs
+++ test/BasicWrapperTool.Tests/ResultExtensionsTests.cs
@@ -1,6 +1,7 @@
 namespace BasicWrapperTool.Tests
 {
     using Moq;
+    using System;
     using Xunit;
 
     public class ResultExtensionsTests
@@ -9,24 +10,93 @@ namespace BasicWrapperTool.Tests
         public void FromMaybe_WithMaybeWithValueAndResultWithValueSuccess_ResultWithValueSuccess()
         {
             // Arrange
-            var maybeMock = new Mock<IMaybe<string>>();
+            Mock<IMaybe<string>> maybeMock = new Mock<IMaybe<string>>();
             maybeMock.Setup(m => m.Value).Returns("3");
             maybeMock.Setup(m => m.HasValue).Returns(true);
             maybeMock.Setup(m => m.HasNoValue).Returns(false);
 
-            var resultMock = new Mock<IResult<int>>();
+            Mock<IResult<int>> resultMock = new Mock<IResult<int>>();
             resultMock.Setup(m => m.Value).Returns(3);
             resultMock.Setup(m => m.IsSuccess).Returns(true);
             resultMock.Setup(m => m.IsFail).Returns(false);
 
             // Act
-            var actual = maybeMock.Object.FromMaybe("input cannot be null", () => resultMock.Object);
+            IResult<int> actual = maybeMock.Object.FromMaybe("input cannot be null", () => resultMock.Object);
 
             // Assert
             Assert.NotNull(actual);
             Assert.True(actual.IsSuccess);
             Assert.False(actual.IsFail);
             Assert.Equal(3, actual.Value);
+        }
+
+        [Fact]
+        public void Select_WithFunctionSource_ReturnResultSuccess()
+        {
+            // Arrange
+            Result<string> result = Result<string>.Success("test");
+            Result<int> result2 = Result<int>.Success(123);
+            Func<string, int> func = x => result2.Value;
+
+            // Act
+            IResult<int> actual = result.Select(func);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.IsType<Result<int>>(actual);
+            Assert.True(actual.IsSuccess);
+            Assert.False(actual.IsFail);
+        }
+
+        [Fact]
+        public void SelectMany_WithFunctionResultSource_ReturnResultSuccess()
+        {
+            // Arrange
+            Result<string> result = Result<string>.Success("test");
+            Result<int> result2 = Result<int>.Success(123);
+            Func<string, Result<int>> func = x => result2;
+
+            // Act
+            IResult<int> actual = result.SelectMany(func);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.IsType<Result<int>>(actual);
+            Assert.True(actual.IsSuccess);
+            Assert.False(actual.IsFail);
+        }
+
+        [Fact]
+        public void ToResult_WithFunctionOutResult_ResultFailWithMessage()
+        {
+            // Arrange
+            Func<int> func = () => Convert.ToInt32("test");
+
+            // Act
+            IResult<int> actual = func.Try();
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.True(actual.IsFail);
+            Assert.False(actual.IsSuccess);
+            Assert.Equal(default(int), actual.Value);
+            Assert.NotEqual(string.Empty, actual.ErrorMessage);
+        }
+
+        [Fact]
+        public void ToResult_WithFunctionOutResult_ResultSuccess()
+        {
+            // Arrange
+            Func<int> func = () => 1;
+
+            // Act
+            IResult<int> actual = func.Try();
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.True(actual.IsSuccess);
+            Assert.False(actual.IsFail);
+            Assert.Equal(1, actual.Value);
         }
     }
 }

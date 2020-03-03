a14d5aee8e92d885d775d62ce3ffd6e9033e6873
seban <seban@DESKTOP-N706T72>
2020-03-03 02:50:56 +0000

    WIP: update tests

--- src/BasicWrapperTool/ResultExtensions.cs
+++ src/BasicWrapperTool/ResultExtensions.cs
@@ -4,22 +4,6 @@
 
     public static class ResultExtensions
     {
-        public static IResult<TResult> EnsureNotNull<TResult>(this Func<TResult> func, string errorMessage)
-            where TResult : class
-        {
-            try
-            {
-                var value = func();
-                return value is null
-                    ? Result<TResult>.Error(errorMessage)
-                    : Result<TResult>.Success(value);
-            }
-            catch
-            {
-                return Result<TResult>.Error(errorMessage);
-            }
-        }
-
         public static IResult<TResult> FromMaybe<TMaybe, TResult>(this IMaybe<TMaybe> maybe,
             string errorMessage,
             Func<IResult<TResult>> func)
@@ -30,50 +14,32 @@
                 : Result<TResult>.Error(errorMessage);
         }
 
-        public static IResult<TResult> Select<TSource, TResult>(this Result<TSource> m, Func<TSource, TResult> f)
+        public static IResult<TResult> Select<TSource, TResult>(this IResult<TSource> result, Func<TSource, TResult> func)
         {
-            return m.Map(f);
+            return result.Map(func);
         }
 
-        public static IResult<TResult> SelectMany<TSource, TResult>(this Result<TSource> m,
-            Func<TSource, Result<TResult>> f)
+        public static IResult<TResult> SelectMany<TSource, TResult>(this IResult<TSource> result,
+            Func<TSource, IResult<TResult>> func)
         {
-            return m.Bind(f);
+            return result.Bind(func);
         }
 
-        public static IResult<TResult> SelectMany<TSource, TM, TResult>(this IResult<TSource> m,
+        public static IResult<TResult> SelectMany<TSource, TM, TResult>(this IResult<TSource> result,
             Func<TSource, IResult<TM>> mSelector, Func<TSource, TM, TResult> rSelector)
         {
-            return m.Bind(v =>
+            return result.Bind(v =>
                 mSelector(v)
                     .Map(tm =>
                         rSelector(v, tm)));
-        }
-
-        public static IResult<TResult> ToResult<TResult>(this Func<TResult> func)
-        {
-            try
-            {
-                return Result<TResult>.Success(func());
-            }
-            catch (Exception e)
-            {
-                return Result<TResult>.Error(e.Message);
-            }
-        }
-
-        public static IResult<T> ToResult<T>(this IMaybe<T> maybe, string errorMessage) where T : class
-        {
-            return maybe.HasValue
-                ? Result<T>.Success(maybe.Value)
-                : Result<T>.Error(errorMessage);
         }
 
         public static IResult<TResult> Try<TResult>(this Func<TResult> func)
         {
             try
             {
-                return Result<TResult>.Success(func());
+                var value = func();
+                return Result<TResult>.Success(value);
             }
             catch (Exception ex)
             {

939326eae4867788aef84b13e46c134b709e2a5d
seban <seban@DESKTOP-N706T72>
2020-05-01 21:24:00 +0100

    Fix Result

--- src/BasicWrapperTool/Maybe.cs
+++ src/BasicWrapperTool/Maybe.cs
@@ -3,19 +3,13 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    public class Maybe<T> : IMaybe<T> where T : class
+    public sealed class Maybe<T> : IMaybe<T> where T : class
     {
         private readonly IEnumerable<T> _values;
 
-        public Maybe()
-        {
-            this._values = new T[0];
-        }
+        public Maybe() => this._values = new T[0];
 
-        public Maybe(T value)
-        {
-            this._values = new[] { value };
-        }
+        public Maybe(T value) => this._values = new[] { value };
 
         public bool HasNoValue => !this.HasValue;
         public bool HasValue => this._values.Any();
--- test/BasicWrapperTool.Tests/ResultWithValueTests.cs
+++ test/BasicWrapperTool.Tests/ResultWithValueTests.cs
@@ -1,6 +1,5 @@
 namespace BasicWrapperTool.Tests
 {
-    using System;
     using Xunit;
 
     public class ResultWithValueTests
@@ -16,32 +15,11 @@ namespace BasicWrapperTool.Tests
             var actual = Result<string>.Fail(failMessage);
 
             // Assert
-            Assert.NotNull(actual);
             Assert.Equal(default(string), actual.Value);
             Assert.Equal(expectedFailMessage, actual.Message);
             Assert.False(actual.IsSuccess);
             Assert.True(actual.IsFail);
         }
-
-        [Fact]
-        public void FailFromException_WithException_ResultFail()
-        {
-            // Assert
-            const string message = "test";
-            var exception = new Exception(message);
-
-            // Act
-            var actual = Result<string>.FailFromException(exception);
-
-            // Assert
-            Assert.NotNull(actual);
-            Assert.Equal(default(string), actual.Value);
-            Assert.Equal(message, actual.Message);
-            Assert.False(actual.IsSuccess);
-            Assert.True(actual.IsFail);
-        }
-
-        //Exception
 
         [Fact]
         public void Success_WithParameters_ResultSuccess()
@@ -55,7 +33,6 @@ namespace BasicWrapperTool.Tests
             var actual = Result<string>.Success(value);
 
             // Assert
-            Assert.NotNull(actual);
             Assert.Equal(expectedValue, actual.Value);
             Assert.Equal(expectedFailMessage, actual.Message);
             Assert.True(actual.IsSuccess);

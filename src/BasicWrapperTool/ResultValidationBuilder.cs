c779c2120009f4b5a345c2678a0c7a08bfaa72db
seban <seban@DESKTOP-N706T72>
2020-05-07 17:56:18 +0100

    ResultValidationBuilder

---
+++ src/BasicWrapperTool/ResultValidationBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace BasicWrapperTool
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ResultValidationBuilder
+    {
+        private readonly IList<IResult> _results = new List<IResult>();
+
+        private IEnumerable<string> Messages => this._results.Where(r => r.IsFail).SelectMany(m => m.Messages);
+        private bool AllSucceed => this._results.All(r => r.IsSuccess);
+
+        public ResultValidationBuilder AddResult<TResult>(IResult<TResult> result)
+        {
+            this._results.Add(result.ToResult());
+            return this;
+        }
+
+        public Result Build()
+        {
+            return this.AllSucceed
+                ? Result.Success()
+                : Result.Fail(this.Messages);
+        }
+
+        public Result<T> Build<T>(T value)
+        {
+            return this.AllSucceed
+                ? Result<T>.Success(value)
+                : Result<T>.Fail(this.Messages);
+        }
+    }
+}
